9c57751d09cd098ef6f074e94fcbc0a82b2d025a
jim <jchoncholas@gmail.com>
2022-01-29 15:03:12 -0500

    better handle 403 from smartrent.py

--- smartrentmqttbridge.py
+++ smartrentmqttbridge.py
@@ -57,6 +57,11 @@ THERMOSTAT_DISCOVERY = ('{'
 class SmartRentBridge:
     async def create():
         self = SmartRentBridge()
+        await self.setupMqtt()
+        await self.setupSmartRent()
+        return self
+
+    async def setupMqtt(self):
         self.mqtt_client = mqtt.Client()
         self.mqtt_client.username_pw_set(MQTT_USER, password=MQTT_PASS)
         if MQTT_TLS is True:
@@ -65,7 +70,7 @@ class SmartRentBridge:
             self.mqtt_client.tls_insecure_set(not MQTT_TLS)
 
         self.mqtt_client.on_connect = self.on_mqtt_connect
-        self.mqtt_client.on_message = self.on_mqtt_message
+        self.mqtt_client.on_message = self.mqttHandleMessage
 
         self.mqtt_client.connect(MQTT_HOST, MQTT_PORT, 60)
         self.mqtt_client.loop_start()
@@ -74,43 +79,53 @@ class SmartRentBridge:
         self.mqtt_client.subscribe(TOPIC_THERM_FAN_MODE + '/set')
         self.mqtt_client.subscribe(TOPIC_THERM_SET_TEMP + '/set')
 
-        api = await async_login(SMARTRENT_EMAIL, SMARTRENT_PASS)
-        print("logged in to smartrent")
-
-        print(str(len(api.get_thermostats())) + " thermostats found")
-        self.thermo = api.get_thermostats()[0]
-        self.thermo.start_updater()
-        self.thermo.set_update_callback(self.therm_on_evt)
-        #print("current temp is " + self.thermo.get_current_temp())
-
-        print(str(len(api.get_locks())) + " locks found")
-        self.lock = api.get_locks()[0]
-        self.lock.start_updater()
-        self.lock.set_update_callback(self.lock_on_evt)
-
-        #print("published test to mqtt")
-        #self.mqtt_client.publish(MQTT_TOPIC_PREFIX + '/testdev/testcmd', "hi2")
-
         print("starting home assistant discovery")
         self.mqtt_client.publish("homeassistant/lock/srlock0/config", LOCK_DISCOVERY, retain=True)
         self.mqtt_client.publish("homeassistant/climate/srclimate0/config", THERMOSTAT_DISCOVERY, retain=True)
-        return self
+
+
+    async def setupSmartRent(self):
+        self.api = await async_login(SMARTRENT_EMAIL, SMARTRENT_PASS)
+        print("logged in to smartrent")
+
+        print(str(len(self.api.get_thermostats())) + " thermostats found")
+        self.thermo = self.api.get_thermostats()[0]
+        self.thermo.start_updater()
+        self.thermo.set_update_callback(self.srThermEvent)
+        #print("current temp is " + self.thermo.get_current_temp())
+
+        print(str(len(self.api.get_locks())) + " locks found")
+        self.lock = self.api.get_locks()[0]
+        self.lock.start_updater()
+        self.lock.set_update_callback(self.srLockEvent)
 
     def on_mqtt_connect(self, client, userdata, flags, rc):
         print("Connected to MQTT broker with result code " + str(rc))
 
-    def on_mqtt_message(self, client, userdata, msg):
+    def mqttHandleMessage(self, client, userdata, msg):
         topic = msg.topic.split('/')
         device = topic[1]
         command = topic[2]
         value = msg.payload.decode()
         print("MQTT message for device: " + device + " command: " + command + " value: " + value)
+        for x in range(3):
+            try:
+                asyncio.run(self.handleMessage(device, command, value))
+                break;
+            except RuntimeError:
+                # smartrent.py raises websockets.exceptions.InvalidStatusCode
+                # with error 403, causes mqtt to throw RuntimeError
+                print("Runtime error. Renewing smartrent api")
+                asyncio.run(self.setupSmartRent())
+        else:
+            raise Exception("smartrent failed to connect 3 times in a row")
 
+    async def handleMessage(self, device, command, value):
         if device == "lock":
             if command == "set":
                 print("setting lock to " + value)
-                asyncio.run(self.lock.async_set_locked(value != "UNLOCK"))
-            self.lock_on_evt() # update state topic
+                await self.lock.async_set_locked(value != "UNLOCK")
+            self.srLockEvent() # update state topic
 
         if device == "thermostat":
             if command == "mode":
@@ -118,53 +133,57 @@ class SmartRentBridge:
                     print("Bad input " + value + " passed to thermostat/mode. Should be heat, cool, auto, or off")
                 else:
                     print("setting thermostat mode to " + value)
-                    asyncio.run(self.thermo.async_set_mode(value))
+                    await self.thermo.async_set_mode(value)
             if command == "fan":
                 if value != "auto" and value != "on":
                     print("Bad input " + value + " passed to thermostat/fan. Should be auto or on")
                 else:
                     print("setting thermostat fan mode to " + value)
-                    asyncio.run(self.thermo.async_set_fan_mode(value))
+                    await self.thermo.async_set_fan_mode(value)
             if command == "setpoint":
                     if self.thermo.get_mode() == "heat":
                         print("setting thermostat heating setpoint to " + value)
-                        asyncio.run(self.thermo.async_set_heating_setpoint(int(float(value))))
+                        await self.thermo.async_set_heating_setpoint(int(float(value)))
                     else:
                         print("setting thermostat cooling setpoint to " + value)
-                        asyncio.run(self.thermo.async_set_cooling_setpoint(int(float(value))))
-            self.therm_on_evt() # update state topic
+                        await self.thermo.async_set_cooling_setpoint(int(float(value)))
+            self.srThermEvent() # update state topic
 
 
-    def therm_on_evt(self):
+    def srThermEvent(self):
         print("thermostat event, publishing to mqtt")
-        self.mqtt_client.publish(TOPIC_THERM_MODE + '/state', self.thermo.get_mode(), retain=True)
-        self.mqtt_client.publish(TOPIC_THERM_FAN_MODE + '/state', self.thermo.get_fan_mode(), retain=True)
+        self.mqtt_client.publish(TOPIC_THERM_MODE + '/state', self.thermo.get_mode())
+        self.mqtt_client.publish(TOPIC_THERM_FAN_MODE + '/state', self.thermo.get_fan_mode())
         if self.thermo.get_mode() == "heat":
-            self.mqtt_client.publish(TOPIC_THERM_SET_TEMP + '/state', self.thermo.get_heating_setpoint(), retain=True)
+            self.mqtt_client.publish(TOPIC_THERM_SET_TEMP + '/state', self.thermo.get_heating_setpoint())
         else:
-            self.mqtt_client.publish(TOPIC_THERM_SET_TEMP + '/state', self.thermo.get_cooling_setpoint(), retain=True)
-        self.mqtt_client.publish(TOPIC_THERM_CUR_TEMP + '/state', self.thermo.get_current_temp(), retain=True)
-        self.mqtt_client.publish(TOPIC_THERM_CUR_HUMI + '/state', self.thermo.get_current_humidity(), retain=True)
+            self.mqtt_client.publish(TOPIC_THERM_SET_TEMP + '/state', self.thermo.get_cooling_setpoint())
+        self.mqtt_client.publish(TOPIC_THERM_CUR_TEMP + '/state', self.thermo.get_current_temp())
+        self.mqtt_client.publish(TOPIC_THERM_CUR_HUMI + '/state', self.thermo.get_current_humidity())
 
-    def lock_on_evt(self):
+    def srLockEvent(self):
         print("lock event, publishing to mqtt. state: " + "LOCKED" if self.lock.get_locked() else "UNLOCKED")
-        self.mqtt_client.publish(TOPIC_LOCK + '/state', "LOCKED" if self.lock.get_locked() else "UNLOCKED", retain=True)
+        self.mqtt_client.publish(TOPIC_LOCK + '/state', "LOCKED" if self.lock.get_locked() else "UNLOCKED")
 
 
 async def main():
-    await SmartRentBridge.create()
     while True:
-        await asyncio.sleep(10)
+        #loop.create_task(SmartRentBridge.create(), loop=loop)
+        sr = await SmartRentBridge.create()
+
+        #loop = asyncio.get_event_loop()
+        #def eh(loop, context):
+        #    # context["message"] will always be there; but context["exception"] may not
+        #    msg = context.get("exception", context["message"])
+        #    print("Caught exception: "+msg)
+
+        #loop.set_exception_handler(eh)
+
+        while True:
+            await asyncio.sleep(10)
 
 if __name__ == "__main__":
-    while True:
-        try:
-            asyncio.run(main())
-        except KeyboardInterrupt:
-            break
-        except websockets.exceptions.InvalidStatusCode:
-            print("websocket timeout")
-            pass
-        except:
-            print("general exception")
-            pass
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
